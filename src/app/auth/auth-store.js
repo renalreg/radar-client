edcffce828163eedf894d63099823d6787dfcdac
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-02-18 15:29:08 +0000

    Document authStore

--- src/app/auth/auth-store.js
+++ src/app/auth/auth-store.js
@@ -3,7 +3,13 @@
 
   var app = angular.module('radar.auth');
 
-  /** Service for storing user credentials (e.g tokens) */
+  /**
+   * Service for storing user credentials (e.g tokens).
+   *
+   * Data is stored in the browser's local storage. In older browsers that
+   * don't implement the local storage API the data is stored in a cookie
+   * instead.
+   */
   function authStore(localStorageService) {
     return {
       logout: logout,
@@ -13,23 +19,44 @@
       getUserId: getUserId
     };
 
+    /** Removes the user ID and token. */
     function logout() {
       localStorageService.remove('userId');
       localStorageService.remove('token');
     }
 
+    /**
+     * Sets the token.
+     *
+     * @param {String} token - Session token.
+     */
     function setToken(token) {
       localStorageService.set('token', token);
     }
 
+    /**
+     * Sets the user ID.
+     *
+     * @param {Number} userId - User ID.
+     */
     function setUserId(userId) {
       localStorageService.set('userId', userId);
     }
 
+    /**
+     * Gets the session token.
+     *
+     * @return {String} Session token.
+     */
     function getToken() {
       return localStorageService.get('token');
     }
 
+    /**
+     * Gets the user ID.
+     *
+     * @return {Number} User ID.
+     */
     function getUserId() {
       var userId = localStorageService.get('userId');
 
