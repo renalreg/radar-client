f62924db20252c7c9a3a81c9502a8de8c2e3deba
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-03-02 16:21:26 +0000

    Comment the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -28,15 +28,18 @@ var lazypipe = require('lazypipe');
 
 var common = require('./common');
 
+// Determines which config file is used in src/app/config (defaults to "production")
 var config = argv.config || 'production';
 gutil.log('config: ' + config + ' (--config NAME)');
 
 function noop() {}
 
+// Delete built files
 gulp.task('clean', function() {
   return del(['dist', '.tmp']);
 });
 
+// Compile SASS to CSS
 gulp.task('sass', function() {
   return gulp.src('src/sass/**/*.scss')
     .pipe(sass({
@@ -47,6 +50,7 @@ gulp.task('sass', function() {
     .pipe(browserSync.reload({stream: true}));
 });
 
+// Lint the JavaScript source code (won't exit on error)
 gulp.task('js', function() {
   return gulp.src('src/app/**/*.js')
     .pipe(jshint())
@@ -57,6 +61,7 @@ gulp.task('js', function() {
     .pipe(browserSync.reload({stream: true}));
 });
 
+// Inject the CSS and JavaScript files into index.html
 gulp.task('inject', function() {
   var appStyles = gulp.src('.tmp/serve/css/**/*.css', {read: false});
   var appScripts = gulp.src(common.JS, {read: false});
@@ -76,6 +81,7 @@ gulp.task('inject', function() {
     .pipe(browserSync.reload({stream: true}));
 });
 
+// Minify the HTML templates and combine them into a single JavaScript file
 gulp.task('templates', function() {
   return gulp.src('src/app/**/*.html')
     .pipe(minifyHtml({
@@ -104,15 +110,18 @@ gulp.task('html', function() {
 
   var templates = gulp.src('.tmp/templates/*.js', {read: false});
 
+  // Concatenate and minify the JavaScript files
   var js = lazypipe()
     .pipe(rev)
     .pipe(uglify);
 
+  // Concatenate and minify the CSS files
   var css = lazypipe()
     .pipe(rev)
     .pipe(replace, '/bower_components/bootstrap-sass/assets/fonts/bootstrap/', '../fonts/')
     .pipe(minifyCss, {compatibility: 'ie8'});
 
+  // Minify the HTML
   var html = lazypipe()
     .pipe(minifyHtml, {
       empty: true,
@@ -137,25 +146,30 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest('dist/fonts'));
 });
 
+// Optimise the copy the images used by JQueryUI
 gulp.task('images-jquery-ui', function() {
   return gulp.src(common.IMAGES_JQUERY_UI)
     .pipe(imagemin({optimizationLevel: 3}))
     .pipe(gulp.dest('dist/images/jquery-ui'));
 });
 
+// Optimise and copy the application's images
 gulp.task('images', gulp.parallel('images-jquery-ui', function() {
   return gulp.src(common.IMAGES)
     .pipe(imagemin({optimizationLevel: 3}))
     .pipe(gulp.dest('dist/images'));
 }));
 
+// Print the size of each built file
 gulp.task('size', function() {
   return gulp.src('dist/**/*')
     .pipe(size({showFiles: true}));
 });
 
+// Build the application for development (no optimisations)
 gulp.task('build', gulp.series(gulp.parallel('js', 'sass'), 'inject'));
 
+// Build the application for distribution (output is optimised)
 gulp.task('build:dist', gulp.series(
   'clean',
   gulp.parallel('js', 'sass', 'templates'),
@@ -164,12 +178,15 @@ gulp.task('build:dist', gulp.series(
   'size'
 ));
 
+// Refresh the page in the web browser
 gulp.task('reload', function(done) {
   browserSync.reload();
   done();
 });
 
+// Watch source files for changes and rebuild intermediate files
 gulp.task('watch', gulp.series('build', function() {
+  // Wrap gulp.watch with logging
   function watch(glob, opt, task) {
     return gulp.watch(glob, opt, task)
       .on('add', function(path) {
@@ -196,6 +213,8 @@ gulp.task('watch', gulp.series('build', function() {
   watch(['src/app/**/*.html', '!src/index.html'], gulp.series('reload'));
 }));
 
+// Proxy requests to NGINX or Express
+// BrowserSync will refresh the page when a source file changes
 gulp.task('browser-sync', function() {
   browserSync.init({
     proxy: 'http://localhost:8082',
@@ -203,9 +222,11 @@ gulp.task('browser-sync', function() {
   });
 });
 
+// Serve the development build with Express
 gulp.task('express', function() {
   var app = express();
 
+  // Rules are checked in the ordered they are defined
   app.use(express.static('.tmp/serve'));
   app.use(express.static('src'));
   app.use('/bower_components', express.static('bower_components'));
@@ -217,6 +238,7 @@ gulp.task('express', function() {
   app.listen(8082);
 });
 
+// Serve the distribution build with Express
 gulp.task('express:dist', function() {
   var app = express();
 
@@ -229,6 +251,7 @@ gulp.task('express:dist', function() {
   app.listen(8081);
 });
 
+// Lint the JavaScript source code (exits on error unlike the "js" task)
 gulp.task('lint', function() {
   return gulp.src('src/app/**/*.js')
     .pipe(jshint())
@@ -238,6 +261,7 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter('fail'));
 });
 
+// Run the tests
 gulp.task('test', function(cb) {
   new karma.Server({
     configFile: __dirname + '/karma.conf.js',
@@ -247,6 +271,7 @@ gulp.task('test', function(cb) {
   }, cb).start();
 });
 
+// Run the tests and produce a test coverage report
 gulp.task('coverage', function(cb) {
   new karma.Server({
     configFile: __dirname + '/karma.conf.js',
@@ -256,6 +281,7 @@ gulp.task('coverage', function(cb) {
   }, cb).start();
 });
 
+// Run the tests with Sauce Labs
 gulp.task('sauce-labs', function(cb) {
   new karma.Server({
     configFile: __dirname + '/sauce-labs.conf.js',
@@ -263,8 +289,12 @@ gulp.task('sauce-labs', function(cb) {
   }, cb).start();
 });
 
+// Build the development build and watch for changes
+// Express serves the files and BrowserSync provides live reload
 gulp.task('serve', gulp.parallel('watch', 'browser-sync', 'express'));
 
+// Build the distribution build and serve the files with Express
 gulp.task('serve:dist', gulp.series('build:dist', 'express:dist'));
 
+// Run the "serve" task by default
 gulp.task('default', gulp.series('serve'));
