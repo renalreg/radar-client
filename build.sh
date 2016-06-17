6ad76926ecca8f87ad9aff288b2c8312558930d3
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-06-17 11:57:58 +0100

    Allow build script to be run from anywhere

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+cd "$(dirname "$0")"
 version=$(node -e "console.log(require('./package.json').version);")
 gulp build:dist
 tar --transform "s/^dist/radar-client-$version/" -czf "radar-client-$version.tar.gz" dist
