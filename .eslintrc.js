3983cbb9ef0abdde63aa79bb6ebb6c5d516703a2
Rupert Bedford <rupert@rupertb.com>
2016-09-16 00:42:48 +0100

    Add commonjs env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true,
-        'jasmine': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  'env': {
+    'browser': true,
+    'es6': true,
+    'jasmine': true,
+    'commonjs': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
