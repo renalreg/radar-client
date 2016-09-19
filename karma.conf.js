e79d4ffc823205e12cc8cfd461d73793b86ba388
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-09-19 15:59:08 +0100

    Add test coverage

--- karma.conf.js
+++ karma.conf.js
@@ -3,19 +3,31 @@ webpack.entry = {};
 
 module.exports = function(config) {
   config.set({
-    basePath: 'src/app',
     frameworks: ['jasmine'],
-    files: ['test.js'],
+    files: ['src/app/test.js'],
     preprocessors: {
-      'test.js': ['webpack', 'sourcemap']
+      'src/app/test.js': ['webpack', 'sourcemap']
     },
-    reporters: ['dots'],
+    reporters: ['dots', 'coverage'],
     port: 9876,
     browsers: ['PhantomJS'],
     webpack: webpack,
     webpackMiddleware: {
       noInfo: true,
       stats: 'errors-only'
+    },
+    coverageReporter: {
+      dir : 'coverage',
+      reporters: [
+        {
+          type: 'html',
+          subdir: 'html'
+        },
+        {
+          type: 'lcov',
+          subdir: 'lcov'
+        }
+      ]
     }
   })
 };
