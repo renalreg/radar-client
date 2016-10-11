152d053aa3f60b925d5f9e66647c597a0b2f0ac8
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-10-11 11:59:27 +0100

    Comment diagnosis selector

--- src/app/patients/diagnoses/diagnosis-selector.directive.js
+++ src/app/patients/diagnoses/diagnosis-selector.directive.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 import templateUrl from './diagnosis-selector.html';
 
+/** User interface for choosing a diagnosis from a list. */
 function diagnosisSelector(store) {
   return {
     require: 'ngModel',
@@ -10,94 +11,126 @@ function diagnosisSelector(store) {
       'patient': '='
     },
     link: function(scope, element, attrs, ngModel) {
+      // Mapping between group id and secondary diagnoses
       var groupDiagnoses = {};
 
+      // Currently selected group (all diagnoses are displayed when set to null)
+      scope.group = null;
+
+      // List of groups with at least one secondary diagnosis (displayed as tabs)
+      scope.groups = [];
+
+      // Currently selected diagnosis
+      scope.diagnosis = null;
+
+      // Currently displayed diagnoses
+      scope.diagnoses = [];
+
+      // True while the directive is loading
+      scope.loading = true;
+
+      // Add functions to scope
+      scope.setGroup = setGroup;
+      scope.isActive = isActive;
+      scope.use = use;
+      scope.drop = drop;
+
+      /** Called when the model is updated outside of the directive. */
+      ngModel.$render = function() {
+        scope.diagnosis = ngModel.$viewValue;
+      };
+
+      load();
+
+      /** Get the list of diagnoses for this group. */
       function getDiagnoses(group) {
         var key = group === null ? null : group.id;
         return groupDiagnoses[key] || [];
       }
 
-      scope.group = null;
-      scope.groups = [];
-      scope.diagnosis = null;
-      scope.diagnoses = [];
-      scope.loading = true;
-
-      ngModel.$render = function() {
-        scope.diagnosis = ngModel.$viewValue;
-      };
-
-      scope.setGroup = function(group) {
+      /** Set the current group (triggered by clicking a tab). */
+      function setGroup(group) {
         scope.group = group;
         scope.diagnoses = getDiagnoses(group);
-      };
+      }
 
-      scope.isActive = function(group) {
+      /** Returns true if the group is selected. */
+      function isActive(group) {
         return scope.group === group;
-      };
+      }
 
-      scope.use = function(diagnosis) {
-        update(diagnosis);
-      };
-
-      scope.drop = function() {
-        update(null);
-      };
-
+      /** Update the selected diagnosis. */
       function update(diagnosis) {
         scope.diagnosis = diagnosis;
+
+        // Update the model
         ngModel.$setViewValue(diagnosis);
       }
 
-      // Load diagnoses
-      store.findMany('diagnoses').then(function(diagnoses) {
-        // Add a diagnosis
-        function add(group, diagnosis) {
-          var key = group === null ? null : group.id;
+      /** Select a diagnosis. */
+      function use(diagnosis) {
+        update(diagnosis);
+      }
 
-          if (groupDiagnoses[key] === undefined) {
-            groupDiagnoses[key] = [];
+      /** Deselect the diagnosis. */
+      function drop() {
+        update(null);
+      }
+
+      /** Add a diagnosis to the mapping */
+      function add(group, diagnosis) {
+        var key = group === null ? null : group.id;
+
+        if (groupDiagnoses[key] === undefined) {
+          groupDiagnoses[key] = [];
+        }
+
+        // Diagnoses are sorted by weight by default. The diagnosis name is used if two diagnoses
+        // have the same weight.
+        var weight = group === null ? diagnosis.name : [diagnosis.getWeight(group.id), diagnosis.name];
+
+        groupDiagnoses[key].push({
+          diagnosis: diagnosis,
+          edtaCode: diagnosis.getEdtaCode(),
+          weight: weight
+        });
+      }
+
+      /** Load the list of diagnoses. */
+      function load() {
+        store.findMany('diagnoses').then(function(diagnoses) {
+          _.forEach(diagnoses, function(diagnosis) {
+            // Add the diagnosis to the all list
+            add(null, diagnosis);
+
+            // Add the diagnosis to each of its groups
+            _.forEach(diagnosis.groups, function(group) {
+              if (group.type.id === 'SECONDARY') {
+                // Add the diagnosis to the group list
+                add(group.group, diagnosis);
+              }
+            });
+          });
+
+          // Get the patient's groups sorted by name
+          scope.groups = _.sortBy(scope.patient.getGroups(), 'shortName');
+
+          // Remove groups that don't have any diagnoses
+          scope.groups = _.filter(scope.groups, function(group) {
+            return getDiagnoses(group).length > 0;
+          });
+
+          // Select the first group with diagnoses (otherwise all)
+          if (scope.groups.length > 0) {
+            setGroup(scope.groups[0]);
+          } else {
+            setGroup(null);
           }
 
-          var weight = group === null ? diagnosis.name : [diagnosis.getWeight(group.id), diagnosis.name];
-
-          groupDiagnoses[key].push({
-            diagnosis: diagnosis,
-            edtaCode: diagnosis.getEdtaCode(),
-            weight: weight
-          });
-        }
-
-        _.forEach(diagnoses, function(diagnosis) {
-          // Add diagnosis to all
-          add(null, diagnosis);
-
-          _.forEach(diagnosis.groups, function(group) {
-            if (group.type.id === 'SECONDARY') {
-              // Add diagnosis to group
-              add(group.group, diagnosis);
-            }
-          });
+          // Finished loading
+          scope.loading = false;
         });
-
-        // Get's patients groups sorted by name
-        scope.groups = _.sortBy(scope.patient.getGroups(), 'shortName');
-
-        // Remove groups that don't have any diagnoses
-        scope.groups = _.filter(scope.groups, function(group) {
-          return getDiagnoses(group).length > 0;
-        });
-
-        // Default to first group with diagnoses (otherwise all)
-        if (scope.groups.length > 0) {
-          scope.setGroup(scope.groups[0]);
-        } else {
-          scope.setGroup(null);
-        }
-
-        // Finished loading
-        scope.loading = false;
-      });
+      }
     }
   };
 }
