25498be8f5629a81ea8cc56accd15a1c06aa3388
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-10-11 10:32:51 +0100

    Diagnoses admin moved to radar-admin

--- src/app/diagnoses/index.js
+++ src/app/diagnoses/index.js
@@ -1,35 +1,14 @@
 import angular from 'angular';
 
-import {
-  diagnosisPermissionFactory,
-  diagnosesControllerFactory,
-  diagnosesComponent
-} from './diagnoses-component.directive';
-import {
-  diagnosisGroupsControllerFactory,
-  diagnosisGroupsComponent
-} from './groups-component.directive';
 import diagnosisModelFactory from './diagnosis-model';
 
-import templateUrl from './diagnoses.html';
-
-function config($stateProvider, storeProvider) {
+function config(storeProvider) {
   storeProvider.registerModel('diagnoses', 'DiagnosisModel');
-
-  $stateProvider.state('diagnoses', {
-    url: '/diagnoses',
-    templateUrl: templateUrl
-  });
 }
 
-config.$inject = ['$stateProvider', 'storeProvider'];
+config.$inject = ['storeProvider'];
 
 export default angular.module('radar.diagnoses', [])
   .config(config)
-  .factory('DiagnosisPermission', diagnosisPermissionFactory)
-  .factory('DiagnosesController', diagnosesControllerFactory)
-  .directive('diagnosesComponent', diagnosesComponent)
-  .factory('DiagnosisGroupsController', diagnosisGroupsControllerFactory)
-  .directive('diagnosisGroupsComponent', diagnosisGroupsComponent)
   .factory('DiagnosisModel', diagnosisModelFactory)
   .name;
