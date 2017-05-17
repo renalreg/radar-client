9ae60490d066f55e2f3d1af0ec2cf59089cf6401
Rapolas Kaselis <rapolas.kaselis@renalregistry.nhs.uk>
2017-05-17 13:57:37 +0100

    default values for nurture tubes

--- src/app/patients/nurture-samples/nurture-samples-component.directive.js
+++ src/app/patients/nurture-samples/nurture-samples-component.directive.js
@@ -1,4 +1,5 @@
-import templateUrl from './nurture-samples-component.html'
+import _ from 'lodash';
+import templateUrl from './nurture-samples-component.html';
 
 function nurtureSamplesPermissionFactory(PatientObjectPermission) {
   return PatientObjectPermission;
@@ -16,6 +17,7 @@ function nurtureSamplesControllerFactory(
   function NurtureSamplesController($scope) {
     var self = this;
 
+    var originalOptions = null;
 
     $injector.invoke(ModelListDetailController, self, {
       $scope: $scope,
@@ -23,94 +25,157 @@ function nurtureSamplesControllerFactory(
     });
 
     self.load(firstPromise([
-      store.findMany('samples', {patient: $scope.patient.id}).then(function(samples) {
-        return samples;
-        // self.items = samples;
-      }),
+      store.findMany('samples', {patient: $scope.patient.id}),
       store.findMany('samples-protocol-options').then(function(protocolOptions) {
+        originalOptions = _.cloneDeep(protocolOptions);
         $scope.protocolOptions = protocolOptions;
       })
     ]));
 
-    // $scope.protocolOptions = [{
-      // 'id': 'ADULT',
-      // 'label': 'Adult',
-      // 'epa': 18
-    // },
-       // {'id': 'CHILDREN30+B', 'label': 'Children 30+ First', 'epa': 11}
-    // ]
+    $scope.saveAndView = function() {
+      $scope.item.epa = $scope.item.protocol.epa;
+      $scope.item.epb = $scope.item.protocol.epb;
+      $scope.item.lpa = $scope.item.protocol.lpa;
+      $scope.item.lpb = $scope.item.protocol.lpb;
+      $scope.item.uc = $scope.item.protocol.uc;
+      $scope.item.ub = $scope.item.protocol.ub;
+      $scope.item.ud = $scope.item.protocol.ud;
+      $scope.item.fub = $scope.item.protocol.fub;
+      $scope.item.sc = $scope.item.protocol.sc;
+      $scope.item.sa = $scope.item.protocol.sa;
+      $scope.item.sb = $scope.item.protocol.sb;
+      $scope.item.rna = $scope.item.protocol.rna;
+      $scope.item.wb = $scope.item.protocol.wb;
 
-    // $scope.saveAndView = function() {
-    //   console.log($scope.item.protocol.epa);
+      return self.save().then(function(item) {
+        self.view(item);
+      });
+    };
 
-    //   var self = this;
-    //   console.log(self);
-    //   console.log(self.item);
-    //   self.item.epa = $scope.item.protocol.epa.value;
-    //   self.scope.item.epa = $scope.item.protocol.value;
-    //   self.scope.item.protocol = $scope.item.protocol.label;
-    //   console.log(self.scope.item);
+    $scope.edit = function(item) {
+      $scope.protocolOptions = _.cloneDeep(originalOptions);
 
-    //   return self.save().then(function(item) {
-    //     self.view(item);
-    //   });
+      var ok = $scope.item === null || !$scope.editing || !$scope.item.isDirty() ||
+      self.discardChanges();
 
-    // }
+      if (!ok) {
+        return;
+      }
 
+      $scope.viewing = false;
+      $scope.editing = true;
+      $scope.originalItem = item;
+      $scope.item = item.clone();
+
+      if ($scope.item.protocol) {
+        $scope.item.protocol.epa = $scope.item.epa;
+        $scope.item.protocol.epb = $scope.item.epb;
+        $scope.item.protocol.lpa = $scope.item.lpa;
+        $scope.item.protocol.lpb = $scope.item.lpb;
+        $scope.item.protocol.uc = $scope.item.uc;
+        $scope.item.protocol.ub = $scope.item.ub;
+        $scope.item.protocol.ud = $scope.item.ud;
+        $scope.item.protocol.fub = $scope.item.fub;
+        $scope.item.protocol.sc = $scope.item.sc;
+        $scope.item.protocol.sa = $scope.item.sa;
+        $scope.item.protocol.sb = $scope.item.sb;
+        $scope.item.protocol.rna = $scope.item.rna;
+        $scope.item.protocol.wb = $scope.item.wb;
+      }
+    };
 
     $scope.create = function() {
-      var item = store.create('samples', {patient: $scope.patient.id})
+      var item = store.create('samples', {patient: $scope.patient.id});
       $scope.edit(item);
+    };
+
+    function getProtocol() {
+      return $scope.item.protocol.id;
     }
 
-    // function setOptionValue(label, value) {
-    //   for (var i = 0; i < $scope.protocolOptions.length; i++) {
-    //     if ($scope.protocolOptions[i].label === label) {
-    //       console.log($scope.protocolOptions[i].value);
-    //       $scope.protocolOptions[i].value = value;
-    //       console.log($scope.protocolOptions[i].value);
-    //       break;
-    //     }
-    //   }
-    // }
-
-    // $scope.edit = function(item) {
-    //   if (item.epa) {
-    //     setOptionValue('epa', item.epa);
-    //   }
-    //   console.log(item.protocol);
-    //   self.edit(item);
-    // }
-
-
-    // http://plnkr.co/edit/Z2IIVdIaj48oIcZ6bbDj?p=preview
-
-    $scope.showEpa = function() {
+    $scope.showEp = function() {
       if ($scope.item && $scope.item.protocol) {
-
-        var protocol = $scope.item.protocol.id;
-        switch(protocol) {
-          case 'ADULT':
-            // $scope.item.epa = 18;
-            return true;
-
-          case 'CHILDREN30+B':
-            // $scope.item.epa = 11;
-            return true;
-
-          case 'CHILDREN30+2ND':
-            // $scope.item.epa = 10;
-            return true;
-          case 'CHILDREN15+B':
-            // $scope.item.epa = 7;
-            return true;
-          default:
-            // $scope.item.epa = null;
-        }
+        var protocol = getProtocol();
+        return (
+          protocol === 'ADULT' ||
+          protocol === 'CHILDREN30_B' ||
+          protocol === 'CHILDREN30_2ND' ||
+          protocol === 'CHILDREN15_B'
+        );
       }
       return false;
-    }
+    };
 
+    $scope.showLp = function() {
+      if ($scope.item && $scope.item.protocol) {
+        var protocol = getProtocol();
+        return (
+          protocol === 'ADULT' ||
+          protocol === 'CHILDREN30_B' ||
+          protocol === 'CHILDREN30_2ND'
+        );
+      }
+      return false;
+    };
+
+    $scope.showU = function() {
+      if ($scope.item && $scope.item.protocol) {
+        var protocol = getProtocol();
+        return (
+          protocol === 'ADULT' ||
+          protocol === 'CHILDREN30_B' ||
+          protocol === 'CHILDREN15_B' ||
+          protocol === 'CHILDREN_LESS_15_B'
+        );
+      }
+      return false;
+    };
+
+    $scope.showFub = function() {
+      return $scope.showU();
+    };
+
+    $scope.showSc = function() {
+      return $scope.showU();
+    };
+
+    $scope.showSa = function() {
+      if ($scope.item && $scope.item.protocol) {
+        return getProtocol() !== 'CHILDREN_LESS_15_2ND';
+      }
+      return false;
+    };
+
+    $scope.showSb = function() {
+      if ($scope.item && $scope.item.protocol) {
+        return getProtocol() === 'ADULT';
+      }
+      return false;
+    };
+
+    $scope.showRna = function() {
+      if ($scope.item && $scope.item.protocol) {
+        var protocol = getProtocol();
+        return (
+          protocol === 'ADULT' ||
+          protocol === 'CHILDREN30_2ND'
+        );
+      }
+      return false;
+    };
+
+    $scope.showWb = function() {
+      if ($scope.item && $scope.item.protocol) {
+        var protocol = getProtocol();
+        return (
+          protocol === 'ADULT' ||
+          protocol === 'CHILDREN30_2ND' ||
+          protocol === 'CHILDREN15_2ND' ||
+          protocol === 'CHILDREN_LESS_15_2ND'
+        );
+      }
+      return false;
+    };
   }
 
   NurtureSamplesController.$inject = ['$scope'],
