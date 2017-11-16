3c5f23b1f56c70ec88b250b766006f7d2d6963f2
Rapolas Kaselis <Rapolas.Kaselis@renalregistry.nhs.uk>
2017-11-16 14:25:25 +0000

    filter recruited by field to show relevant to a user hospitals only

--- src/app/forms/fields/group-field.directive.js
+++ src/app/forms/fields/group-field.directive.js
@@ -2,23 +2,36 @@ import sortGroups from '../../groups/sort-groups';
 
 import templateUrl from './group-field.html';
 
-function frmGroupField(store) {
+function frmGroupField(store, session) {
   return {
     restrict: 'A',
     scope: {
       model: '=',
       required: '&',
-      params: '='
+      params: '=',
+      userOnly: '&?',
+      type: '@?'
     },
     templateUrl: templateUrl,
     link: function(scope) {
-      store.findMany('groups', scope.params).then(function(groups) {
+      var user = session.user;
+      if (scope.userOnly && !user.isAdmin) {
+        var groups = [];
+        for (var i = 0; i < user.groups.length; i++) {
+          if (user.groups[i].group.type === scope.type) {
+            groups.push(user.groups[i].group);
+          }
+        }
         scope.groups = sortGroups(groups);
-      });
+      } else {
+        store.findMany('groups', scope.params).then(function(groups) {
+          scope.groups = sortGroups(groups);
+        });
+      }
     }
   };
 }
 
-frmGroupField.$inject = ['store'];
+frmGroupField.$inject = ['store', 'session'];
 
 export default frmGroupField;
