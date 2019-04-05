28e93bd6725eb16c0e70aa4f7e00e09beb5a2ebe
Rapolas Kaselis <Rapolas.Kaselis@renalregistry.nhs.uk>
2019-04-05 09:56:16 +0100

    add another set of hla fields

--- src/app/forms/hla.directive.js
+++ src/app/forms/hla.directive.js
@@ -5,39 +5,54 @@ function frmHla() {
     template: '<div ng-transclude></div>',
     link: function(scope, element, attrs, ngModel) {
       scope.hla = {
-        a: null,
-        b: null,
-        dr: null
+        a1: null,
+        a2: null,
+        b1: null,
+        b2: null,
+        dr1: null,
+        dr2: null
       };
 
       ngModel.$render = function() {
         var value = ngModel.$viewValue;
 
         if (value === null || value === undefined) {
-          scope.hla.a = null;
-          scope.hla.b = null;
-          scope.hla.dr = null;
+          scope.hla.a1 = null;
+          scope.hla.a2 = null;
+          scope.hla.b1 = null;
+          scope.hla.b2 = null;
+          scope.hla.dr1 = null;
+          scope.hla.dr2 = null;
         } else {
           var parts = value.split('/');
-          scope.hla.a = parts[0].substring(1);
-          scope.hla.b = parts[1].substring(1);
-          scope.hla.dr = parts[2].substring(2);
+          scope.hla.a1 = parts[0].substring(1);
+          scope.hla.a2 = parts[1].substring(1);
+          scope.hla.b1 = parts[2].substring(1);
+          scope.hla.b2 = parts[3].substring(1);
+          scope.hla.dr1 = parts[4].substring(2);
+          scope.hla.dr2 = parts[5].substring(2);
         }
       };
 
-      scope.$watch('hla.a', update);
-      scope.$watch('hla.b', update);
-      scope.$watch('hla.dr', update);
+      scope.$watch('hla.a1', update);
+      scope.$watch('hla.a2', update);
+      scope.$watch('hla.b1', update);
+      scope.$watch('hla.b2', update);
+      scope.$watch('hla.dr1', update);
+      scope.$watch('hla.dr2', update);
 
       function update() {
-        var a = parseInt(scope.hla.a);
-        var b = parseInt(scope.hla.b);
-        var dr = parseInt(scope.hla.dr);
+        var a1 = parseInt(scope.hla.a1);
+        var a2 = parseInt(scope.hla.a2);
+        var b1 = parseInt(scope.hla.b1);
+        var b2 = parseInt(scope.hla.b2);
+        var dr1 = parseInt(scope.hla.dr1);
+        var dr2 = parseInt(scope.hla.dr2);
         let value = null;
-        if (a >= 0 && b >= 0 && dr >= 0) {
-          value = 'A' + a + '/B' + b + '/DR' + dr;
-        // } else if (a >= 0 || b >= 0 || dr >= 0)  {
-        //   ngModel.$setValidity('hla', false);
+        if (a1 >= 0 && b1 >= 0 && dr1 >= 0 && a2 >= 0 && b2 >= 0 && dr2 >= 0) {
+          value = 'A' + a1 + '/A' + a2 + '/B' + b1 + '/B' + b2 + '/DR' + dr1 + '/DR' + dr2;
+          // } else if (a >= 0 || b >= 0 || dr >= 0)  {
+          //   ngModel.$setValidity('hla', false);
         } else {
           value = null;
         }
