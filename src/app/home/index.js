4c1ef07ce730f6e4c20d7804c5cc23d2615f77b1
OliveraReeves <oliver.reeves@ukkidney.org>
2025-12-09 10:33:05 +0000

    feat(RD-3): Remove outdated information from RaDaR demo and live web interface home pages
    live and staging/demo/local have different homepage texts

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -1,19 +1,30 @@
 import angular from 'angular';
-
 import templateUrl from './home.html';
 
 function config($stateProvider) {
   $stateProvider.state('index', {
     url: '/',
     templateUrl: templateUrl,
-    data: {
-      public: true
-    }
+    controller: 'HomeController as vm',
+    data: { public: true }
   });
 }
 
 config.$inject = ['$stateProvider'];
 
-export default angular.module('radar.home', [])
+function HomeController($window) {
+  const vm = this;
+
+  const url = $window.location.href.toLowerCase();
+
+  // isLive = true unless URL contains demo or staging
+  vm.isLive = !(url.includes('demo') || url.includes('staging')|| url.includes('local'));
+}
+
+HomeController.$inject = ['$window'];
+
+export default angular
+  .module('radar.home', [])
   .config(config)
+  .controller('HomeController', HomeController)
   .name;
